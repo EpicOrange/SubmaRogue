25c2f74f96804d867c04306cc310d5c98cc5f68a
EpicOrange@github.com <danieldoesntcheckthis@gmail.com>
2017-03-25 21:25:20 -0700

    Add log.js

--- game.js
+++ game.js
@@ -1,20 +1,26 @@
 
+var width = 60;
+var height = 45;
 var Game = {
-  width: 50,
-  height: 50,
+  log: new Log(width),
   player: null,
   scheduler: null,
   engine: null,
   init: function() {
-    this.display = new ROT.Display({width:this.width,height:this.height});
+    this.display = new ROT.Display({
+      width: width,
+      height: height,
+      forceSquareRatio: true
+    });
+    document.body.appendChild(this.log.display.getContainer());
     document.body.appendChild(this.display.getContainer());
+
     this.scheduler = new ROT.Scheduler.Speed();
 
-    this.map = new Map(this.width, this.height);
+    this.map = new Map(width, height);
     this.map.generateTiles();
     this.map.generateEntities(this);
     this.map.drawAll();
-
     this.map.addItem({color:'green',char:'X',name:'test',type:'weapon',value:10});
 
     this.engine = new ROT.Engine(this.scheduler);
---
+++ log.js
@@ -0,0 +1,26 @@
+class Log {
+  constructor(width) {
+    this.logHeight = 5;
+    this.width = width;
+    this.display = new ROT.Display({
+      width: this.width,
+      height: this.logHeight + 1,
+      forceSquareRatio: true
+    });
+    this.messages = [];
+    this.update();
+  }
+  update() {
+    for (let i = 0; i < this.messages.length; i++) {
+      this.display.drawText(0, this.logHeight - i - 1, this.messages[i]);
+    }
+    this.display.drawText(0, this.logHeight, "-".repeat(this.width));
+  }
+  add(message) {
+    this.messages.unshift(message); // add to front
+    if (this.messages.length > this.logHeight) {
+      this.messages.pop(); // remove from back
+    }
+    this.update();
+  }
+}
